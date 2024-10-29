0e232d7a5d8f97cb71a1cf6cbeda34ef07d938ad
MuhammadMahdiAmirpour <muhammadmahdiamirpour@gmail.com>
2024-10-29 21:04:19 +0330

    init project

---
+++ src/main/java/com/github/robustapi/RobustApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.robustapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobustApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobustApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/robustapi/RobustApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.robustapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobustApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
