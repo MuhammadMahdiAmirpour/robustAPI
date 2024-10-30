d42a14c7f670b6111cc091895cefc5f72ea7d00a
MuhammadMahdiAmirpour <muhammadmahdiamirpour@gmail.com>
2024-10-30 23:36:45 +0330

    set up RabbitMQ successfully

---
+++ src/main/java/com/github/robustapi/model/User.java
@@ -0,0 +1,30 @@
+package com.github.robustapi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Builder
+public class User {
+	@Id
+	@Column(unique = true)
+	@JsonProperty("nationalId")
+	private String nationalId;
+	@JsonProperty("firstName") private String firstName;
+	@JsonProperty("lastName") private String lastName;
+	@JsonProperty("birthDate") private String birthDate;
+	@JsonProperty("address") private String address;
+	@Column(unique = true)
+	@JsonProperty("phoneNumber")
+	private String phoneNumber;
+}
