c87d67a652825a12abd873b3b6ac066e97724f45
MuhammadMahdiAmirpour <muhammadmahdiamirpour@gmail.com>
2024-11-03 18:55:56 +0330

    removed redundant directory

--- db/init-database.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-
-# Run SQL scripts
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" < /docker-entrypoint-initdb.d/1.sql
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" < /docker-entrypoint-initdb.d/2.sql
-
--- db/mysql-init/01-create-database.sql
+++
@@ -1,3 +0,0 @@
-CREATE
-DATABASE IF NOT EXISTS robustapidb;
-USE robustapidb;
